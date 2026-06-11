2a5aa8b4fd6a951c932922941d70976cb0028c3d
joaoibira-ux <joao.ibira@gmail.com>
2026-06-11 01:14:11 -0300

    v3.0/func-v17: link voltar ao menu aponta para sistema.gwrevestimentos.com.br

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const VERSION = "func-v16";
+const VERSION = "func-v17";
 const ASSETS = [
   "./index.html",
   "./style.css?v=11",
-  "./app.js?v=16",
+  "./app.js?v=17",
   "./manifest.json",
   "./Logo-gw.png"
 ];
